2bdd3bd1b799a903538a473469866722e9a4b63f
Samuel Odebaran <samuel_odebaran@hotmail.com>
2023-09-20 20:32:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const milliseconds = document.querySelector("#milliseconds");
 const seconds = document.querySelector("#seconds");
 const minutes = document.querySelector("#minutes");
 const hours = document.querySelector("#hours");
+const input = document.getElementById("neon");
+const button = document.querySelectorAll(".button");
 
 let changeHours = 0;
 let changeMinutes = 0;
@@ -64,4 +66,51 @@ function resume() {
     milliseconds.textContent = "000";
     seconds.textContent = "00";
     minutes.textContent = "00";
+}
+
+//Alterando as cores do meus elementos ao se escolher uma das paletas disponíveis
+
+function palletNeon() {
+    const corFundo = document.querySelector(".main__clock");
+    const format = document.querySelector(".clock__format");
+    const time = document.querySelectorAll(".time");
+    const separator = document.querySelectorAll(".separator");
+    const header = document.querySelector(".header");
+    corFundo.style.backgroundColor = "#1E1E1E";
+    format.style.borderColor = "#0085ff";
+    time.forEach((element) => {
+        element.style.color = "#69b4ff";
+    });
+    button.forEach((element) => {
+        element.style.backgroundColor = "#006fff";
+        element.style.color = "#FFFFFF";
+    });
+    separator.forEach((element) => {
+        element.style.color = "#69b4ff";
+    });
+
+    header.style.backgroundColor = "#2d2d2d";
+}
+
+function palletSaphire() {
+    const corFundo = document.querySelector(".main__clock");
+    const format = document.querySelector(".clock__format");
+    const time = document.querySelectorAll(".time");
+    const separator = document.querySelectorAll(".separator");
+    const header = document.querySelector(".header");
+
+    corFundo.style.backgroundColor = "#0f1c2e";
+    format.style.borderColor = "#2c3a4f";
+    time.forEach((element) => {
+        element.style.color = "#cee8ff";
+    });
+    button.forEach((element) => {
+        element.style.backgroundColor = "#1f3a5f";
+        element.style.color = "#cee8ff";
+    });
+    separator.forEach((element) => {
+        element.style.color = "#cee8ff";
+    });
+
+    header.style.backgroundColor = "#1f2b3e";
 }
